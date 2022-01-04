52673ff2d7a4981c631606babc3df98d3defd94c
zhaojinghao <zhaojinghao1996@gmail.com>
2022-01-04 15:09:17 -0800

    Update windows scripts example

--- docker-cs118-run-windows.sh
+++ docker-cs118-run-windows.sh
@@ -15,6 +15,6 @@ winpty docker run --interactive --rm --privileged -e DISPLAY \
 #              -p 8080:8080 \
 #              --tmpfs=/curr/${USER}:exec,mode=755,uid=$(id -u),gid=$(id -g) \
 #              --tty \
-#              --volume=/C:/Users/zhaoj/Desktop/winter22-project0/setup.sh:/opt/docker-cs118/setup.sh:ro \
-#              --volume=/C:/Users/zhaoj/Desktop/winter22-project0/project:/project:rw \
+#              --volume=/C:/Users/zhaoj/Desktop/winter22-project0/setup-windows.sh:/opt/docker-cs118/setup.sh:ro \
+#              --volume=/C:/Users/zhaoj/Desktop/winter22-project0:/project:rw \
 #              ubuntu:20.04

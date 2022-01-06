1f36f9f2f9e85061340b9e8172e6e19b080a952b
Varun Patil <radialapps@gmail.com>
2022-01-05 19:54:52 -0800

    Remove old instructions

--- docker-cs118-run-windows.sh
+++ docker-cs118-run-windows.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Replace "Replace with your folder path" to your project folder path, such as C:/Users/zhaoj/Desktop/winter22-project0
-
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 WINDOWSPATH=$(echo $SCRIPTPATH | sed 's/^\///' | sed 's/\//\\/g' | sed 's/^./\0:/')
 
@@ -14,12 +12,3 @@ winpty docker run --interactive --privileged -e DISPLAY \
              --volume=$WINDOWSPATH/setup-windows.sh:/opt/docker-cs118/setup.sh:ro \
              --volume=$WINDOWSPATH:/project:rw \
              ubuntu:20.04
-
-# ====================== Example ===============================
-# winpty docker run --interactive --rm --privileged -e DISPLAY \
-#              -p 8080:8080 \
-#              --tmpfs=/curr/${USER}:exec,mode=755,uid=$(id -u),gid=$(id -g) \
-#              --tty \
-#              --volume=/C:/Users/zhaoj/Desktop/winter22-project0/setup-windows.sh:/opt/docker-cs118/setup.sh:ro \
-#              --volume=/C:/Users/zhaoj/Desktop/winter22-project0:/project:rw \
-#              ubuntu:20.04
